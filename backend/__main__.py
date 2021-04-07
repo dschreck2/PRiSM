82cc7d7df3a9f25d441c63504b36ca4574b8dc31
Dane Warren <daneswarren@gmail.com>
2021-04-07 18:03:02 -0400

    Add logging to main

--- backend/__main__.py
+++ backend/__main__.py
@@ -2,8 +2,13 @@
 The main file to store PRiSM data in the database
 """
 
+from core import generate_log as logger
+from core import read_config
+
+@logger.wrap(logger.enter, logger.exit)
 def main():
-    print("Hello, World!")
+    if read_config.config["is_running"]:
+        print("Hello, World!")
 
 if __name__ == '__main__':
     main()
