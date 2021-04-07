b51c540cc41645f446b4bf544226e98b13ca6159
Dane Warren <daneswarren@gmail.com>
2021-04-07 18:02:49 -0400

    Logging support

---
+++ backend/core/generate_log.py
@@ -0,0 +1,72 @@
+"""Logging file to create a global Logger instance"""
+
+import logging
+import sys
+from logging import handlers
+
+from core import read_config
+
+
+def create_file_handler(log_name):
+    """
+    creates file logging output
+    - log_name: (string) the config file
+    """
+    file_handler = handlers.TimedRotatingFileHandler(
+        log_name, when="midnight", interval=1
+    )
+    file_handler.suffix = "%Y%m%d"
+    file_handler.setFormatter(formatter)
+    return file_handler
+
+
+def traceback_hook(type, value, traceback):
+    """
+    Prints stack trace to log if an uncaught error is encountered
+    """
+    logger.error("Uncaught Error:", exc_info=(type, value, traceback))
+
+
+def wrap(pre, post):
+    """ Wrapper """
+
+    def decorate(func):
+        """ Decorator """
+
+        def call(*args, **kwargs):
+            """ Actual wrapping """
+            pre(func)
+            result = func(*args, **kwargs)
+            post(func)
+            return result
+
+        return call
+
+    return decorate
+
+
+def enter(func):
+    """ Pre function logging """
+    logger.debug("{}.{} ->".format(func.__code__.co_filename, func.__name__))
+
+
+def exit(func):
+    """ Post function logging """
+    logger.debug("{}.{} <-".format(func.__code__.co_filename, func.__name__))
+
+
+logger = logging.getLogger("PRiSM")
+formatter = logging.Formatter(read_config.config["log"]["format"])
+
+logger.addHandler(
+    create_file_handler(read_config.config["log"]["location"])
+)
+if read_config.config["is_debug"]:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+logger.debug(
+    "Log file located: {}".format(read_config.config["log"]["location"])
+)
+sys.excepthook = traceback_hook
+logger.debug("Logger Initialized")
