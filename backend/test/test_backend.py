8099b6edfa1211094dda3fb0c668dc2803d63699
Dane Warren <daneswarren@gmail.com>
2021-05-02 19:20:25 -0400

    Conversion tests

--- backend/test/test_backend.py
+++ backend/test/test_backend.py
@@ -1,5 +1,37 @@
-# pipenv shell
-# python pytest
-def test_pytest():
-    # Example
-    assert 1 == 1
+"""
+This file is for unit testing of the backend
+"""
+# Imports used to test
+import os
+import sys
+
+# Set testing path to find backend files
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+
+# Imports to test
+from core import conversion
+
+
+def test_conversion_blocks_to_gb():
+    """
+    Tests core.conversion.blocks_to_gb()
+    """
+    blocks = 976562500
+    assert conversion.blocks_to_gb(blocks) == 500
+
+
+def test_conversion_bytes_to_gb():
+    """
+    Tests core.conversion.bytes_to_gb()
+    """
+    bytes = 17179869184
+    assert conversion.bytes_to_gb(bytes) == 16
+
+
+def test_conversion_pages_to_gb():
+    """
+    Tests core.conversion.pages_to_gb()
+    """
+    pages = 524288
+    assert conversion.pages_to_gb(pages) == 2
